19ad42dd0b0b6bda9425ece89635c3c1346ce662
asaxena3079 <asaxena3079@gmail.com>
2016-06-11 19:00:16 +0530

    Added check for login for admin and user

--- web/src/main/ts/application/login.ts
+++ web/src/main/ts/application/login.ts
@@ -1,6 +1,6 @@
 ///<reference path="../../../../node_modules/angular2/typings/browser.d.ts"/>
 
-import {Component} from "@angular/core";
+import {Component} from '@angular/core';
 
 @Component({
     selector: 'login',
@@ -8,4 +8,20 @@ import {Component} from "@angular/core";
 })
 
 export class Login{
+
+    private userName: string;
+    private passWord: string;
+
+    tryLogin(){
+        console.log("Hi"+ this.userName + " " + this.passWord);
+        if(this.userName==="admin" && this.passWord==="pwd"){
+            alert("Done Admin");
+        }
+        else if(this.userName==="user" && this.passWord==="pwd"){
+            alert("Done User");
+        }
+        else{
+            alert("Sorry");
+        }
+    }
 }
