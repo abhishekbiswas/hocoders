fe2c681311409d05816604816464e8ae715e21ee
Shubham Utwal <shubhamutwal@gmail.com>
2016-06-11 17:09:13 +0530

    Segregated component for navbar from base template

--- web/src/main/ts/application/app.component.ts
+++ web/src/main/ts/application/app.component.ts
@@ -1,12 +1,13 @@
 ///<reference path="../../../../node_modules/angular2/typings/browser.d.ts"/>
 
 import {Component} from '@angular/core';
+import {ROUTER_DIRECTIVES} from '@angular/router';
+import {NavBar} from './navbar';
 
 @Component({
     selector: 'my-app',
-    template: `
-    Hello World!
-    `
+    directives: [NavBar, ROUTER_DIRECTIVES],
+    templateUrl: 'templates/application.html'
 })
 export class AppComponent {
     constructor(){
---
+++ web/src/main/ts/application/navbar.ts
@@ -0,0 +1,10 @@
+///<reference path="../../../../node_modules/angular2/typings/browser.d.ts"/>
+
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'navbar',
+    templateUrl: 'templates/navbar.html'
+})
+export class NavBar {
+}
